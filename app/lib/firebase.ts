646079fc89a62a99c369d4218a6e73912e656696
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2025-07-18 13:48:53 +0100

    feat: configuration stage

---
+++ app/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { initFirestore } from '@auth/firebase-adapter'
+import { cert } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+// import { getStorage } from 'firebase-admin/storage'
+import 'server-only'
+
+export const firestore = initFirestore({
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
+  })
+  //storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+})
+
+export const db = getFirestore()
+// export const storage = getStorage().bucket()
+// Uncomment the line below if you need to use Firebase Storage
