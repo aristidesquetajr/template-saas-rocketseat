477e9722fffdc74fbedb9859d7840da01eaedb0e
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2025-07-30 22:49:48 +0100

    feat: SEO metadata

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import { handleAuth } from '@/app/actions/handle-auth'
 import { auth } from '@/app/lib/auth'
+import { Metadata } from 'next'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
 
 export default async function Dashboard() {
   const session = await auth()
